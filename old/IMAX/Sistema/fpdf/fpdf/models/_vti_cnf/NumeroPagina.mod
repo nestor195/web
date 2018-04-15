8940a5fe4f5520e0ad293bad4a8c586066d43f24
Nestor Taborda <38335114+nestor195@users.noreply.github.com>
2018-04-14 22:34:20 -0300

    optimización
    se borraron archivos no utilizados

--- old/IMAX/Sistema/fpdf/fpdf/models/_vti_cnf/NumeroPagina.mod
+++
@@ -1,6 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|15 Nov 2003 00:11:46 -0000
-vti_extenderversion:SR|12.0.0.4518
-vti_cacheddtm:TX|15 Nov 2003 00:11:46 -0000
-vti_filesize:IR|228
-vti_backlinkinfo:VX|
--- old/IMAX/aspnet_client/system_web/1_1_4322/_vti_cnf/SmartNav.js
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|20 Feb 2003 22:14:04 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_lineageid:SR|{C69439C7-1AFF-46E9-8B18-7CFFFD28035A}
-vti_backlinkinfo:VX|
-vti_modifiedby:SR|NESTOR-PC\\Nestor
-vti_cacheddtm:TX|20 Feb 2003 22:14:04 -0000
-vti_filesize:IR|8728
